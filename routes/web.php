a03402430b9e0fd18cd281cbae7cf830f3fa1dd6
keanbarz <keanbareng@gmail.com>
2025-05-27 16:24:43 +0800

    Migrated Project Acic to its own Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ImportController;
+use App\Http\Controllers\AcicController;
 use App\Http\Middleware\CheckFieldOffice;
 use Illuminate\Support\Facades\Route;
 
@@ -11,12 +12,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [ImportController::class, 'dashboard'] )->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/text', [ImportController::class, 'sampletext'] )->middleware([CheckFieldOffice::class]);
+Route::get('/text', [ImportController::class, 'sampletext'] )->middleware([CheckFieldOffice::class]); //Experimental
 
 Route::get('/peppcodes/generate-pdf', [Importcontroller::class, 'generatePDF']);
-Route::post('/importacic/pdf', [Importcontroller::class, 'acicPDF']);
-Route::get('/importacic/del', [Importcontroller::class, 'acicdel']);
-Route::get('/importacic/txt', [Importcontroller::class, 'acictxt'])->name('acictxt');;
 
 Route::get('/peppcodes/notify', [Importcontroller::class, 'notify'])->middleware(['auth', 'verified'])->name('notify');
 Route::get('/peppcodes/export', [Importcontroller::class, 'export'])->middleware(['auth', 'verified'])->name('export');
@@ -25,9 +23,14 @@ Route::get('/peppcodes/filter', [ImportController::class, 'filter'])->middleware
 
 //*upload, figuring out how to update with pepp code tagging
 Route::get('/peppcodes/update', function () {return view('update');})->middleware([CheckFieldOffice::class])->name('update');
-
 Route::post('/import', [ImportController::class, 'import'])->middleware([CheckFieldOffice::class])->name('import');
-Route::post('/importacic', [ImportController::class, 'importacic'])->middleware([CheckFieldOffice::class])->name('importacic');
+
+//Project ACIC (Now moved to a Separate Controller)
+Route::get('/acic', [Aciccontroller::class, 'acic'])->middleware(['auth', 'verified'])->name('acic');
+Route::post('/importacic/pdf', [aciccontroller::class, 'acicPDF']);
+Route::get('/importacic/del', [aciccontroller::class, 'acicdel']);
+Route::get('/importacic/txt', [aciccontroller::class, 'acictxt'])->name('acictxt');
+Route::post('/importacic', [aciccontroller::class, 'importacic'])->middleware([CheckFieldOffice::class])->name('importacic');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -35,8 +38,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Project ACIC
-Route::get('/acic', [Importcontroller::class, 'acic'])->middleware(['auth', 'verified'])->name('acic');
 
 
 require __DIR__.'/auth.php';
